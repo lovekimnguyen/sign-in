952298c30545d7416dcc833fba1dbfd9eeb6c421
Hà Văn Nguyên <122628379+lovekimnguyen@users.noreply.github.com>
2023-01-26 16:23:18 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -4,14 +4,18 @@ function log(){
     if( a=='' && b !='') alert("Chưa điền tên đăng nhập");
     else if( a !='' && b =='') alert("Chưa điền mật khẩu");
     else if( a=='' && b =='') alert("Chưa điền thông tin đăng nhập");
-    else if(a != 'lovekimnguyen' || b != 'lovehanguyen'){
-        alert("Sai tên đăng nhập hoặc mật khẩu");
-    }
-    else {
+    else if(a =='lovekimnguyen' && b=='lovehanguyen') {
         alert("Đăng nhập thành công");
         location.href = 'https://lovekimnguyen.github.io/lovehanguyen';
     }
+    else if(a =='oggy' && b=='lovehanguyen') {
+        alert("Đăng nhập thành công");
+        location.href = 'https://lovekimnguyen.github.io/oggy';
+    }
+    else {
+        alert('Sai tên đăng nhập hoặc mật khẩu');
+    }
 }
 function forgot(){
-    alert("Username : lovekimnguyen     Password : lovehanguyen" );
+    location.href = ' https://lovekimnguyen.github.io/forgot__password/';
 }
